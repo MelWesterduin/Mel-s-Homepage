2d09bdfc49eb5c6b7b07d034ebef404fe1a48e0c
Mel Westerduin <m.westerduin1@gmail.com>
2023-06-20 20:46:13 +0200

    some refactoring

--- homepage/src/components/App/App.js
+++ homepage/src/components/App/App.js
@@ -1,9 +1,7 @@
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header"></header>
+    <div>
+      <header></header>
     </div>
   );
 }
